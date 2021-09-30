82619f95a6af6cdfad72303d217fcec5e98cc2ba
steve kim <tofusoup429@gmail.com>
2021-09-30 15:47:38 +0900

    ggg

--- dist/ImageView.js
+++ dist/ImageView.js
@@ -1,43 +1,17 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = __importStar(require("react"));
-var Select_1 = __importDefault(require("@material-ui/core/Select"));
-var MenuItem_1 = __importDefault(require("@material-ui/core/MenuItem"));
+var react_1 = __importDefault(require("react"));
 var Button_1 = __importDefault(require("@material-ui/core/Button"));
 var ImagesView = function (_a) {
     var imagesBase64Array = _a.imagesBase64Array, width = _a.width, uploadMergedImages = _a.uploadMergedImages;
-    var _b = react_1.useState(2), howManyImagesOnWidth = _b[0], handleHowManyImagesOnWidth = _b[1];
-    var handleHowManyImagesOnWidthWrapper = function (e) { return handleHowManyImagesOnWidth(parseInt(e.target.value)); };
     return (react_1.default.createElement("div", { style: { position: "relative" } }, imagesBase64Array.length > 0 &&
         react_1.default.createElement("div", { id: "Images", style: { display: "flex", flexDirection: "row", justifyContent: "center", flexWrap: 'wrap' } },
             imagesBase64Array.map(function (imageBase64, index) {
-                return (imageBase64.length > 10 && react_1.default.createElement("img", { key: index, src: imageBase64, width: width * 0.95 / howManyImagesOnWidth, alt: 'NoImage' }));
+                return (imageBase64.length > 10 && react_1.default.createElement("img", { key: index, src: imageBase64, width: width * 0.95 / 2, alt: 'NoImage' }));
             }),
-            react_1.default.createElement(Select_1.default, { value: howManyImagesOnWidth, onChange: handleHowManyImagesOnWidthWrapper, style: { position: "absolute", top: "15px", left: '15px', zIndex: 1000, backgroundColor: "#f50057" } },
-                react_1.default.createElement(MenuItem_1.default, { value: 1 }, "1 x 1"),
-                react_1.default.createElement(MenuItem_1.default, { value: 2 }, "2 x 2")),
             react_1.default.createElement(Button_1.default, { onClick: uploadMergedImages, size: "small", style: { position: "absolute", top: "40px", left: '15px', zIndex: 1000, backgroundColor: "#f50057" } }, "Upload"))));
 };
 exports.default = ImagesView;
--- src/ImageView.tsx
+++ src/ImageView.tsx
@@ -1,17 +1,13 @@
-import React, {useState} from 'react';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 
 interface Props{
     imagesBase64Array:string[]
     width:number,
-    uploadMergedImages?:()=>void,
+    uploadMergedImages?:()=>void
 }
 
-const ImagesView = ({imagesBase64Array, width, uploadMergedImages}:Props) =>{
-    const [howManyImagesOnWidth, handleHowManyImagesOnWidth] = useState<number>(2);
-    const handleHowManyImagesOnWidthWrapper = (e:any) => handleHowManyImagesOnWidth(parseInt(e.target.value))    
+const ImagesView = ({imagesBase64Array, width, uploadMergedImages}:Props) =>{ 
     return(
         <div style={{position:"relative"}}>
         {imagesBase64Array.length>0 &&
@@ -19,14 +15,11 @@ const ImagesView = ({imagesBase64Array, width, uploadMergedImages}:Props) =>{
                 {
                     imagesBase64Array.map((imageBase64, index)=>{
                         return(
-                            imageBase64.length>10 && <img key={index} src={imageBase64} width={width*0.95/howManyImagesOnWidth} alt='NoImage'/>
+                            imageBase64.length>10 && <img key={index} src={imageBase64} width={width*0.95/2} alt='NoImage'/>
                         )
                     })
                 }
-                <Select value={howManyImagesOnWidth} onChange={handleHowManyImagesOnWidthWrapper} style={{position:"absolute", top:"15px", left:'15px', zIndex:1000, backgroundColor:"#f50057"}}>
-                    <MenuItem value={1}>1 x 1</MenuItem>
-                    <MenuItem value={2}>2 x 2</MenuItem>
-                </Select>
+                
                 <Button onClick={uploadMergedImages} size="small" style={{position:"absolute", top:"40px", left:'15px', zIndex:1000, backgroundColor:"#f50057"}}>Upload</Button>
             </div>
         }
